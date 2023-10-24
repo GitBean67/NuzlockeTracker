d16010a26fc745a9acdeefe7e634c37882231557
gitBean <hseabolt@css.edu>
2023-10-24 11:36:23 -0500

    Sprint 2 - Added functionality to the new game button to open the second activity.

--- app/src/main/java/com/example/nuzlocketracker/MainActivity.java
+++ app/src/main/java/com/example/nuzlocketracker/MainActivity.java
@@ -1,14 +1,40 @@
 package com.example.nuzlocketracker;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button buttonNewGame;
+    Button buttonLoadGame;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonLoadGame = findViewById(R.id.buttonLoadGame);
+
+        setupNewGameButton();
+    }
+
+    private void setupNewGameButton() {
+        buttonNewGame = findViewById(R.id.buttonNewGame);
+
+        buttonNewGame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("CIS 3334", "New Game button clicked");   // log button click for debugging using "CIS 3334" tag
+                //Call Tracker Activity
+                Intent resultsActIntent = new Intent(MainActivity.this, TrackerActivity.class);
+
+                startActivity(resultsActIntent); //if no result is returned.
+            }
+        });
     }
 }
--- app/src/main/java/com/example/nuzlocketracker/TrackerActivity.java
+++ app/src/main/java/com/example/nuzlocketracker/TrackerActivity.java
@@ -2,13 +2,53 @@ package com.example.nuzlocketracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class TrackerActivity extends AppCompatActivity {
+
+    Button buttonNew;
+    Button buttonSave;
+    Button buttonDelete;
+    EditText editTextSpecies;
+    EditText editTextNickname;
+    EditText editTextLocation;
+    EditText editTextStatus;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tracker);
+
+        setupNewButton();
+        setupSaveButton();
+        setupDeleteButton();
+    }
+
+    private void setupNewButton() {
+        buttonNew = findViewById(R.id.buttonNew);
+        buttonNew.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("CIS 3334", "New Game button clicked");   // log button click for debugging using "CIS 3334" tag
+
+                String species = editTextSpecies.getText().toString();
+                String nickname = editTextNickname.getText().toString();
+                String location = editTextLocation.getText().toString();
+                String status = editTextStatus.getText().toString();
+            }
+        });
+    }
+
+    private void setupSaveButton() {
+        buttonSave = findViewById(R.id.buttonSave);
+    }
+    private void setupDeleteButton() {
+        buttonDelete = findViewById(R.id.buttonDelete);
     }
 }
